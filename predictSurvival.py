6cfd9c78dcdbc29e88033840cfef357d033f5808
unknown <pooja.s1494@gmail.com>
2018-08-16 16:58:11 -0500

    Predicting on test data

--- predictSurvival.py
+++ predictSurvival.py
@@ -1,15 +1,19 @@
 import preprocessing as pre
 import numpy as np
+import pandas as pd
 from sklearn import model_selection
 from sklearn import tree
 from sklearn import ensemble
 #convert our dataframe from pandas to numpy  and specify input and output
-df = pre.df
+df = pre.createDataFrame("train.csv")
+testdf = pre.createDataFrame("test.csv")
+#testdf.info()
 #print(df.head)
 X = df.values
 y = df['Survived'].values
 # remove output field from input data
 X = np.delete(X,1,axis = 1)
+X_results = testdf.values
 #splitting dataset into training and testing
 X_train, X_test, y_train, y_test = model_selection.train_test_split(X,y,test_size=0.3,random_state=0)
 
@@ -33,4 +37,11 @@ gradientBoosting = ensemble.GradientBoostingClassifier(n_estimators=50)
 gradientBoosting.fit(X_train,y_train)
 gbscore = gradientBoosting.score(X_test,y_test)
 print("Gradient boosting score: " + str(gbscore))
+
+#predict on test data
+gradientBoosting.fit(X,y)
+y_results = gradientBoosting.predict(X_results)
+output = np.column_stack((X_results[:,0],y_results))
+df_results = pd.DataFrame(output.astype('int'),columns = ['PassengerId','Survived'])
+df_results.to_csv('titanic_results.csv',index = False)
 
--- preprocessing.py
+++ preprocessing.py
@@ -1,21 +1,22 @@
 import pandas as pd
-df = pd.read_csv("train.csv",header = 0)
-# dropping columns that might not contribute to prediction like name,ticket no ,cabin etc
-cols = ['Name','Ticket','Cabin']
-df = df.drop(cols,axis = 1) #axis = 1 means column, axis = 0 means row
-#drop all rows that has missing  NaN
-#df = df.dropna()
-# quantify all categorical variables or variables with more than 2 options
-dummy = []
-cols = ['Pclass','Sex','Embarked']
-for col in cols:
-    dummy.append(pd.get_dummies(df[col]))
-titanic_dummies = pd.concat(dummy,axis=1)
-#titanic_dummies.info()
-df = pd.concat((df,titanic_dummies),axis=1)
-# remove duplicate columns ie Pclass,Sex,Embarked
-df = df.drop(cols,axis = 1)
-# handling missing age values using interpolate
-df['Age'] = df['Age'].interpolate()
-
-
+def createDataFrame(input):
+    df = pd.read_csv(input,header = 0)
+    # dropping columns that might not contribute to prediction like name,ticket no ,cabin etc
+    cols = ['Name', 'Ticket', 'Cabin']
+    df = df.drop(cols, axis=1)  # axis = 1 means column, axis = 0 means row
+    # drop all rows that has missing  NaN
+    # df = df.dropna()
+    # quantify all categorical variables or variables with more than 2 options
+    dummy = []
+    cols = ['Pclass', 'Sex', 'Embarked']
+    for col in cols:
+        dummy.append(pd.get_dummies(df[col]))
+    titanic_dummies = pd.concat(dummy, axis=1)
+    # titanic_dummies.info()
+    df = pd.concat((df, titanic_dummies), axis=1)
+    # remove duplicate columns ie Pclass,Sex,Embarked
+    df = df.drop(cols, axis=1)
+    # handling missing age values using interpolate
+    df['Age'] = df['Age'].interpolate()
+    df['Fare'] = df['Fare'].interpolate()
+    return df
